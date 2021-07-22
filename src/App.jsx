d450edaa35e85a8d2239a5ccdd43cdefd029a7cd
Mr Sam Law <sam@chongland.com>
2021-07-22 12:55:26 +1000

    Navigation Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
 import "./app.scss";
 
 import { useState } from "react";
-import Navbar from "./components/navbar/Navbar";
-import Header from "./components/header/Header";
-import Projects from "./components/projects/Projects";
-import Footer from "./components/footer/Footer";
-import Menu from "./components/menu/Menu";
-import Contact from "./components/contact/Contact";
-import NewNav from "./components/newNav/NewNav";
+import Navigation from "./components/Navigation";
 
 export default function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState("Home");
 
+  const renderSection = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Blog") {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
   const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <main className="app">
-      <NewNav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
       <div className="sections">
-        <Header />
+        {renderSection()}
+        {/* <Header />
         <Projects />
-        <Contact />
+        <Contact /> */}
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </main>
   );
 }
