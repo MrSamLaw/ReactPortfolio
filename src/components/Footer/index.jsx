0b88c905149eee27c6fff836f564e86f866c0881
Mr Sam Law <sam@chongland.com>
2021-09-11 21:31:05 +1000

    re-style footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -17,30 +17,21 @@ const Footer = () => {
           ^
         </Link>
       </div>
-      <div className="nav__right">
-        <SiGithub
-          className="icon"
-          onClick={() => window.open("https://github.com/MrSamLaw/", "_blank")}
-        />
+      <div className="footer__socials">
         <SiLinkedin
           className="icon"
           onClick={() =>
             window.open("https://www.linkedin.com/in/mrsamlaw/", "_blank")
           }
         />
+        <SiGithub
+          className="icon"
+          onClick={() => window.open("https://github.com/MrSamLaw/", "_blank")}
+        />
       </div>
-      <p className="copyright">&copy; Copyright 2021</p>
       <p className="copyright">
-        <Link
-          className="nav__logo"
-          to="intro"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={400}
-        >
-          SAM LAW
-        </Link>
+        <sup>&copy; </sup>2021
+        <span className="logo">SAM LAW</span>
       </p>
     </footer>
   );
